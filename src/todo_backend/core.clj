a31ca438a9b39cb244b688943519cec118025b1c
Michael SALIHI <admin@prestance-informatique.fr>
2020-04-23 00:56:42 +0200

    Allow CORS

--- src/todo_backend/core.clj
+++ src/todo_backend/core.clj
@@ -1,10 +1,15 @@
 (ns todo-backend.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.cors :refer [wrap-cors]]))
 
 (defn app [req]
   {:status 200
    :body "ok"})
 
+(def handler
+  (wrap-cors app :access-control-allow-origin [#".*"]
+                 :access-control-allow-methods [:get :put :post :delete]))
+
 (defn -main [port]
-  (jetty/run-jetty #'app {:port (Integer. port)
-                          :join? false}))
+  (jetty/run-jetty #'handler {:port (Integer. port)
+                              :join? false}))
