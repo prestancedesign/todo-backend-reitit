a787730b538b395465fc60d85ee478ac9deed9a6
Michael SALIHI <admin@prestance-informatique.fr>
2020-04-23 01:33:27 +0200

    Begin basic routing

--- src/todo_backend/core.clj
+++ src/todo_backend/core.clj
@@ -1,15 +1,27 @@
 (ns todo-backend.core
   (:require [ring.adapter.jetty :as jetty]
-            [ring.middleware.cors :refer [wrap-cors]]))
+            [ring.middleware.cors :refer [wrap-cors]]
+            [reitit.ring :as ring]))
 
-(defn app [req]
+(defn ok [body]
   {:status 200
-   :body "ok"})
+   :body body})
+
+(def app-routes
+  (ring/ring-handler
+   (ring/router
+    [["/" {:get {:handler (fn [req] (ok "OK GET"))}
+           :post {:handler (fn [req] (ok "OK POST"))}}]
+     ["/todos" {:post {:handler (fn [req] (ok "OK POST"))}}]])))
 
 (def handler
-  (wrap-cors app :access-control-allow-origin [#".*"]
-                 :access-control-allow-methods [:get :put :post :delete]))
+  (wrap-cors app-routes :access-control-allow-origin [#".*"]
+                        :access-control-allow-methods [:get :put :post :delete]))
 
 (defn -main [port]
   (jetty/run-jetty #'handler {:port (Integer. port)
                               :join? false}))
+
+(comment
+  (def server (jetty/run-jetty #'handler {:port 3000
+                                          :join? false})))
