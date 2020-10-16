4b646f64f45f480d1d92ba4dca336a15bb81e30d
Michael SALIHI <admin@prestance-informatique.fr>
2020-10-16 10:37:06 +0200

    Refactor to replace Ring JSON middleware with Muuntaja

--- src/todo_backend/core.clj
+++ src/todo_backend/core.clj
@@ -1,11 +1,11 @@
 (ns todo-backend.core
-  (:require reitit.coercion.schema
+  (:require [reitit.coercion.schema :as rcs]
             [reitit.ring :as ring]
             [reitit.ring.coercion :as rrc]
+            [reitit.ring.middleware.muuntaja :as rrmm]
+            [muuntaja.core :as m]
             [ring.adapter.jetty :as jetty]
             [ring.middleware.cors :refer [wrap-cors]]
-            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
-            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [schema.core :as s]
             [todo-backend.store :as store]))
 
@@ -23,10 +23,10 @@
   (ring/ring-handler
    (ring/router
     [["/todos" {:get (fn [req] (ok (map #(append-todo-url % req) (store/get-all-todos))))
-                :post (fn [{:keys [body] :as req}] (-> body
-                                                      store/create-todos
-                                                      (append-todo-url req)
-                                                      ok))
+                :post (fn [{:keys [body-params] :as req}] (-> body-params
+                                                             store/create-todos
+                                                             (append-todo-url req)
+                                                             ok))
                 :delete (fn [_] (store/delete-all-todos)
                           {:status 204})
                 :options (fn [_] {:status 200})}]
@@ -34,21 +34,20 @@
                     :get (fn [{:keys [parameters] :as req}] (-> (store/get-todo (get-in parameters [:path :id]))
                                                                (append-todo-url req)
                                                                ok))
-                    :patch (fn [{:keys [parameters body] :as req}] (-> body
-                                                                      (store/update-todo (get-in parameters [:path :id]))
-                                                                      (append-todo-url req)
-                                                                      ok))
+                    :patch (fn [{:keys [parameters body-params] :as req}] (-> body-params
+                                                                             (store/update-todo (get-in parameters [:path :id]))
+                                                                             (append-todo-url req)
+                                                                             ok))
                     :delete (fn [{:keys [parameters]}] (store/delete-todos (get-in parameters [:path :id]))
                               {:status 204})}]]
-    {:data {:coercion reitit.coercion.schema/coercion
-            :middleware [rrc/coerce-response-middleware
-                         rrc/coerce-request-middleware
+    {:data {:muuntaja m/instance
+            :coercion rcs/coercion
+            :middleware [rrmm/format-middleware
                          rrc/coerce-exceptions-middleware
-                         wrap-keyword-params
-                         wrap-json-response
-                         [wrap-json-body {:keywords? true}]
+                         rrc/coerce-response-middleware
+                         rrc/coerce-request-middleware
                          [wrap-cors :access-control-allow-origin [#".*"]
-                                    :access-control-allow-methods [:get :put :post :patch :delete]]]}})
+                          :access-control-allow-methods [:get :put :post :patch :delete]]]}})
 
    (ring/create-default-handler
     {:not-found (constantly {:status 404 :body "Not found"})})))
