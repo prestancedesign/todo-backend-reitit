c2b8e63bf2170112f742f02961d257f0d89c0a89
Michael SALIHI <admin@prestance-informatique.fr>
2020-10-20 15:25:53 +0200

    Fix migrate for Heroku deployment

--- src/todo_backend/migration.clj
+++ src/todo_backend/migration.clj
@@ -3,12 +3,8 @@
             [ragtime.repl :as repl]))
 
 (def config
-  {:datastore (jdbc/sql-database "jdbc:postgresql:todos?user=postgres&password=mypass")
+  {:datastore (jdbc/sql-database (System/getenv "JDBC_DATABASE_URL"))
    :migrations (jdbc/load-resources "migrations")})
 
 (defn migrate []
   (repl/migrate config))
-
-(comment
- (repl/migrate config)
- (repl/rollback config))
