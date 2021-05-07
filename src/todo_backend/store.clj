3d56222cae188f653cf863c8019770c68e3b607d
Michael SALIHI <admin@prestance-informatique.fr>
2021-05-07 18:31:07 +0200

    Move next-jdbc option globally

--- src/todo_backend/store.clj
+++ src/todo_backend/store.clj
@@ -4,9 +4,9 @@
             [next.jdbc.result-set :as rs]
             [next.jdbc.sql :as sql]))
 
-(def db (System/getenv "JDBC_DATABASE_URL"))
+(def ds (jdbc/get-datasource (System/getenv "JDBC_DATABASE_URL")))
 
-(def ds (jdbc/get-datasource db))
+(def db (jdbc/with-options ds {:builder-fn rs/as-unqualified-lower-maps}))
 
 (defn as-row [row]
   (rename-keys row {:order :position}))
@@ -15,23 +15,20 @@
   (rename-keys row {:position :order}))
 
 (defn create-todos [todo]
-  (as-todo (sql/insert! ds :todos (as-row todo)
-                        {:builder-fn rs/as-unqualified-lower-maps})))
+  (as-todo (sql/insert! db :todos (as-row todo))))
 
 (defn get-todo [id]
-  (as-todo (sql/get-by-id ds :todos id
-                          {:builder-fn rs/as-unqualified-lower-maps})))
+  (as-todo (sql/get-by-id db :todos id)))
 
 (defn update-todo [body id]
-  (sql/update! ds :todos (as-row body) {:id id})
+  (sql/update! db :todos (as-row body) {:id id})
   (get-todo id))
 
 (defn delete-todos [id]
-  (sql/delete! ds :todos {:id id}))
+  (sql/delete! db :todos {:id id}))
 
 (defn get-all-todos []
-  (jdbc/execute! ds ["SELECT * FROM todos;"]
-                 {:builder-fn rs/as-unqualified-lower-maps}))
+  (jdbc/execute! db ["SELECT * FROM todos;"]))
 
 (defn delete-all-todos []
-  (sql/delete! ds :todos [true]))
+  (sql/delete! db :todos [true]))
